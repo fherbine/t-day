5966c68254407911905171804181eb68c27ab499
fherbine <felix.herbinet@yahoo.com>
2019-07-01 21:43:58 +0200

    Main Basis

---
+++ main.py
@@ -0,0 +1,12 @@
+'''T-Day App.
+Todolist of the day will be useful for the everyday routine.
+'''
+
+from kivy.app import App
+
+class TDayApp(App):
+    def build(self):
+        pass
+
+if __name__ == '__main__':
+    TDayApp().run()
