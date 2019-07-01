4c776c3bec16cbd957b9fdd254d2e5c0f9458cea
fherbine <felix.herbinet@yahoo.com>
2019-07-01 21:54:56 +0200

    Screenmanager

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 Todolist of the day will be useful for the everyday routine.
 '''
 
+__author__  = 'fherbine'
+
+
 from kivy.app import App
+from kivy.properties import ObjectProperty
+
 
 class TDayApp(App):
+    screenmanager = ObjectProperty(rebind=True)
+
     def build(self):
-        pass
+        self.screenmanager = self.root.screenmanager
+
 
 if __name__ == '__main__':
     TDayApp().run()
