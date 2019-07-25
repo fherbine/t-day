c4dfdc95ef1fd292e6d60346b77c59b9b2b06052
fherbine <felix.herbinet@yahoo.com>
2019-07-25 07:46:50 +0200

    Implementing settings file

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ Todolist of the day will be useful for the everyday routine.
 __author__  = 'fherbine'
 
 
+import json
+
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.properties import (
+    DictProperty,
     NumericProperty,
     ObjectProperty,
+    StringProperty,
 )
 
 from kivy.core.window import Window
@@ -22,17 +26,27 @@ from controllers.meetings import Meetings
 
 
 class TDayApp(App):
-    meetings_controller = ObjectProperty(rebind=True)
+    language = StringProperty()
     screenmanager = ObjectProperty(rebind=True)
+    settings = DictProperty()
+    meetings_controller = ObjectProperty(rebind=True)
     time = NumericProperty()
 
     def build(self):
+        self._load_settings()
+
         self.screenmanager = self.root.screenmanager
         self.meetings_controller = meetings = Meetings()
         meetings.load_meetings()
-        Clock.schedule_interval(self.update_time, 0)
+        Clock.schedule_interval(self._update_time, 0)
 
-    def update_time(self, dt):
+    def _load_settings(self):
+        with open('settings.json') as settings_file:
+            self.settings = settings = json.load(settings_file)
+
+        self.language = settings.get('language', 'en_US')
+
+    def _update_time(self, dt):
         self.time += dt
 
     def on_meetings_controller(self, *_):
